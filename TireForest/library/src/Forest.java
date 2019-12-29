d67d7c517ed56f80cd61a3ce9d36ce0a1229a832
shataowei <taowei.sha@ymm56.com>
2019-12-29 22:58:53 +0800

    tire

---
+++ TireForest/library/src/Forest.java
@@ -0,0 +1,12 @@
+/**
+ * Created by slade on 2019/12/29.
+ */
+public class Forest {
+    private static final long serialVersionUID = -8070767282792075347L;
+
+    public Forest() {
+    }
+
+
+}
+
